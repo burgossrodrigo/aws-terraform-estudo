307eaab26b6042e08360b223955f1c3f7cf688df
Rodrigo Burgos <rodrigoburgos@live.com>
2024-11-03 15:23:28 -0300

    .

--- main.tf
+++ main.tf
@@ -10,7 +10,22 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
   profile = "bia" 
+}
+
+resource "aws_instance" "bia-dev" {
+    ami = "ami-02f3f602d23f1659d"
+    instance_type = "t3.micro"
+    tags = {
+      ambiente = "dev"
+      Name = "bia-dev"
+    }
+
+    vpc_security_group_ids = ["sg-02f9520d9e3d4c1b1"]
+
+    root_block_device {
+        volume_size = 10
+    }
 }
 
