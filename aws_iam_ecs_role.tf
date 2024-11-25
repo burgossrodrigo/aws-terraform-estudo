6cbbac7141bbd8f1868057f1267a14f1eae109b2
Rodrigo Burgos <rodrigoburgos@live.com>
2024-11-25 19:38:13 -0300

    feat: IAM

--- aws_iam_ecs_role.tf
+++ aws_iam_ecs_role.tf
@@ -14,3 +14,14 @@ resource "aws_iam_role" "ecs_instance_role" {
     name               = "ecs-instance-role-tf"
     assume_role_policy = data.aws_iam_policy_document.ecs_instance_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+    role = aws_iam_role.ecs_instance_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+    role = aws_iam_role.ecs_instance_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
