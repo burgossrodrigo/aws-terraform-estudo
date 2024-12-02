74096fd3a34a245d49701b0a3acb01b4098d08fa
Rodrigo Burgos <rodrigoburgos@live.com>
2024-12-02 04:19:00 -0300

    working

--- aws_ecs_task_definition_bia_web.tf
+++ aws_ecs_task_definition_bia_web.tf
@@ -1,20 +1,24 @@
 resource "aws_ecs_task_definition" "bia_web" {
-  family       = "task_def_bia"
-  network_mode = "bridge"
+  family        = "task_def_bia"
+  network_mode  = "bridge"
+  task_role_arn = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([{
     name      = "bia_web"
     image     = "${aws_ecr_repository.bia.repository_url}:latest"
     essential = true
-    portMapping = [{
-      containerPort = 80
+    portMappings = [{
+      containerPort = 8080
       hostPort      = 80
     }]
     cpu               = 1024
     memoryReservation = 400
-    enviroment = [
+    environment = [
       { name = "DB_PORT", value = "5432" },
-      { name = "DB_HOST", value = "${aws_db_instance.bia.address}" }
+      { name = "DB_HOST", value = "${aws_db_instance.bia.address}" },
+      { name = "DB_SECRET_NAME", value = "${data.aws_secretsmanager_secret.bia_db.name}" },
+      { name = "DB_REGION", value = "us-east-1" },
+      { name = "DEBUG_SECRET", value = "true" }
     ]
     logConfiguration = {
       logDriver = "awslogs"
@@ -25,4 +29,9 @@ resource "aws_ecs_task_definition" "bia_web" {
       }
     }
   }])
+
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
 }
